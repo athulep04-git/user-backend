d026b5721d49b7f774080d5794290140d41a9b61
athulep04-git <athulep04@gmail.com>
2026-03-31 23:15:09 +0530

    register login update done

---
+++ controllers/userController.js
@@ -0,0 +1,80 @@
+const user = require("../models/userModel");
+
+exports.addUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const existingUser = await user.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json("user with this email already exist");
+    } else {
+      const newUser = new user({ username, email, password });
+      await newUser.save();
+      res.status(201).json({ message: "user added", newUser });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await user.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json("user not found");
+    } else if (existingUser.password !== password) {
+      return res.status(400).json("incorrect password");
+    } else {
+      res.status(200).json({
+        message: "login successful",
+        existingUser,
+      });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  try {
+    const allUsers = await user.find();
+    res.status(200).json({ message: "users found", allUsers });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getOneUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const oneUser = await user.findById(id);
+    if (oneUser) {
+      res.status(200).json({ message: "user found", oneUser });
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { username, email, password } = req.body;
+
+  try {
+    const upadtedUser = await user.findByIdAndUpdate(
+      id,
+      { username, email, password },
+      { new: true },
+    );
+
+    if (!upadtedUser) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    res.status(200).json({ message: "user details updated", upadtedUser });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
---
+++ router/route.js
@@ -0,0 +1,12 @@
+const express=require('express')
+
+const userController=require('../controllers/userController')
+
+const route=express.Router()
+
+route.post('/api/adduser',userController.addUser)
+route.get('/api/getusers',userController.getUsers)
+route.put('/api/updateuser/:id',userController.updateUser)
+route.post('/api/login',userController.loginUser)
+route.get('/api/getAuser/:id',userController.getOneUser)
+module.exports=route
