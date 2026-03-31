9904825fdf03a52d28bf2108feb21f01d88336e6
athulep04-git <athulep04@gmail.com>
2026-03-31 22:42:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+require("./config/db");
+const route = require("./router/route");
+const server = express();
+server.use(cors());
+server.use(express.json());
+server.use(route);
+const PORT = process.env.PORT || 3000;
+server.get("/", (req, res) => {
+  res.send("welcome to crud server");
+});
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
